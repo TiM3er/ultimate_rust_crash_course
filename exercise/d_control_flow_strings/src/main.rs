8784cf478823334b921cc340dadcedf15263d554
pwitowski <pwitowski@omecon.pl>
2023-02-17 13:03:56 +0100

    fix: done d exercise

--- exercise/d_control_flow_strings/src/main.rs
+++ exercise/d_control_flow_strings/src/main.rs
@@ -1,6 +1,7 @@
 // Silence some warnings so they don't distract from the exercise.
 #![allow(dead_code, unused_mut, unused_variables)]
 
+
 fn main() {
     // This collects any command-line arguments into a vector of Strings.
     // For example:
@@ -19,7 +20,11 @@ fn main() {
         // - If arg is "sum", then call the sum() function
         // - If arg is "double", then call the double() function
         // - If arg is anything else, then call the count() function, passing "arg" to it.
-
+        if arg == "sum" {
+            sum();
+        } else if arg == "double" {
+            double();
+        } else { count(arg) }
 
         // 1b. Now try passing "sum", "double" and "bananas" to the program by adding your argument
         // after "cargo run".  For example "cargo run sum"
@@ -31,6 +36,9 @@ fn sum() {
     // 2. Use a "for loop" to iterate through integers from 7 to 23 *inclusive* using a range
     // and add them all together (increment the `sum` variable).  Hint: You should get 255
     // Run it with `cargo run sum`
+    for i in 7..=23 {
+        sum += i;
+    }
 
 
     println!("The sum is {}", sum);
@@ -42,6 +50,11 @@ fn double() {
     // 3. Use a "while loop" to count how many times you can double the value of `x` (multiply `x`
     // by 2) until `x` is larger than 500.  Increment `count` each time through the loop. Run it
     // with `cargo run double`  Hint: The answer is 9 times.
+    'double_loop: loop {
+        x *= 2;
+        count += 1;
+        if x > 500 { break 'double_loop; }
+    }
 
 
     println!("You can double x {} times until x is larger than 500", count);
@@ -51,8 +64,12 @@ fn count(arg: String) {
     // Challenge: Use an unconditional loop (`loop`) to print `arg` 8 times, and then break.
     // You will need to count your loops, somehow.  Run it with `cargo run bananas`
     //
-    // print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
-
+    let mut count = 0;
+    'count_loop: loop {
+        print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
+        count += 1;
+        if count > 7 { break 'count_loop; }
+    }
 
     println!(); // This will output just a newline at the end for cleanliness.
 }
